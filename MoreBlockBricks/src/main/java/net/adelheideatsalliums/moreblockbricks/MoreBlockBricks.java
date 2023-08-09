6ce6ea977a3c7e098e80aff495c1785331961a61
AdelheidEatsAlliums <109511321+AdelheidEatsAlliums@users.noreply.github.com>
2023-08-09 02:03:13 -0400

    Add files via upload

---
+++ MoreBlockBricks/src/main/java/net/adelheideatsalliums/moreblockbricks/MoreBlockBricks.java
@@ -0,0 +1,15 @@
+package net.adelheideatsalliums.moreblockbricks;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreBlockBricks implements ModInitializer {
+    public static final String MOREBLOCKBRICKS = "moreblockbricks";
+    public static final Logger LOGGER = LoggerFactory.getLogger("MOREBLOCKBRICKS");
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ MoreBlockBricks/src/main/java/net/adelheideatsalliums/moreblockbricks/client/MoreBlockBricksClient.java
@@ -0,0 +1,13 @@
+package net.adelheideatsalliums.moreblockbricks.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreBlockBricksClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
